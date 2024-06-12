314dd9bc93c20dc3c88aac6d274b67a1b3732908
kush <kushshah652@gmail.com>
2024-06-12 09:37:59 -0400

    minor code changes

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -5,7 +5,7 @@ const TaskList = ({ tasks, onEdit, onDelete }) => {
     return (
         <div>
             {tasks.length === 0 ? (
-                <p className="text-center text-gray-600 dark:text-gray-400">No tasks available</p>
+                <p className="text-center text-gray-600 dark:text-gray-400">Create Tasks to Get Started!</p>
             ) : (
                 tasks.map(task => (
                     <Task key={task.id} task={task} onEdit={onEdit} onDelete={onDelete} />
